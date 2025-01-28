68715c5f05482371e9eaa7403882e169769baf6c
Sarah <sarah.makki1223@gmail.com>
2025-01-28 15:48:43 -0500

    Add game functionality

--- game.py
+++ game.py
@@ -1,24 +1,43 @@
 import random
 
 class Game:
-    def __init__(self, dictionary_path):
+    def __init__(self, dictionary_path, lives = 5):
         self.dictionary = Dictionary(dictionary_path)
         self.target = self.dictionary.getTarget()
+        self.lives = lives
 
     def guess(self, word):
+        if self.lives <= 0:
+            return 'game over'
+        
         if not self.dictionary.checkWord(word):
             return 'r'
         elif word == self.target:
             return 'a'
         else:
-            return self.getHint(word)
+            hint = self.getHint(word)
+            self.lives -= 1
+            return hint
         
     def getHint(self, word):
-        return ''
+        hint = ''
+        for idx, letter in enumerate(word):
+            targetLetter = self.target[idx]
+            if letter == targetLetter:
+                hint += '1'
+            else:
+                if letter in self.target:
+                    hint += '0'
+                else:
+                    hint += '-'
+        return hint
+    
+    def hasLives(self):
+        return self.lives > 0
 
 class Dictionary:
     def __init__(self, dictionary_path):
-        self.dictionary = {}
+        self.dictionary = set()
         with open(dictionary_path, 'r') as file:
             for line in file:
                 self.dictionary.add(line.strip())
--- main.py
+++ main.py
@@ -1,4 +1,23 @@
 from game import Game
 
-def __main__():
+def main():
     game = Game('sample-dictionary/a2b036e7ee0777d630571e63ebbeacc3-78b2d0a5e9e371989688ecb27b25ea0c14252ef3/sample_dictionary.txt')
+    print(game.target)
+
+    while game.hasLives():
+        guess = input('Enter your guess: ')
+        hint = game.guess(guess)
+        if hint == 'r':
+            print('Invalid word, guess again')
+        elif hint == 'a':
+            print('Correct! Game won')
+            break
+        else:
+            print('Hint: ', hint)
+
+    if game.hasLives():
+        print('Game won')
+    else:
+        print('Game over')
+
+main()
